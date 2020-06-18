663ff04b8a05a7f193d0e626fff99674ee1246fc
Dudeicle <brian-griffiths@lambdastudents.com>
2020-06-18 19:11:20 -0400

    card

--- microbrewery-app/src/Components/BeerCard.js
+++ microbrewery-app/src/Components/BeerCard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 export default function BeerCard (props) {
-    const { name, address, phoneNumber, url } = props.beers
+    const { name, address, phoneNumber, url } = props.beer
 
-    if(!props.beers) {
+    if(!props.beer) {
         return <h3>Working on fetching your beers....</h3>
     }
 
     return (
         <div>
-            <h2>Name: {props.beers.name}</h2>
-            <p>Address: {props.beers.address}</p>
-            <p>Phone Number: {props.beers.phoneNumber}</p>
-            <p>LINK: {props.beers.url}</p>
+            <h2>Name: {props.beer.name}</h2>
+            <p>Address: {props.beer.address}</p>
+            <p>Phone Number: {props.beer.phoneNumber}</p>
+            <a href={props.beer.url}>Link To Website!</a>
         </div>
     )
 }
